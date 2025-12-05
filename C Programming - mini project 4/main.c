9ab7b7c24bead1f08f8787d24f3d95e074af8223
princelungucell-43 <test@3schools.com>
2025-12-05 13:06:02 +0200

    PROJECT 4

--- C Programming - mini project 4/main.c
+++ C Programming - mini project 4/main.c
@@ -1,14 +1,31 @@
 //
 //  main.c
 //  C Programming - mini project 4
-//
-//  Created by user on 12/10/2025.
+
+//  Created by PRINCE LUNGU (GROUP B)
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    
+    int celsius;
+    int fahrenheit;
+    int finalCel;
+    int finalFah;
+    
+    printf("ENTER TEMPERETURE IN CELSIUS: ");
+    scanf("%d", &celsius);
+    
+    finalFah = celsius * 9/5 + 32;
+    
+    printf("in Fahrenheit : %d\n", finalFah);
+    
+    printf("ENTER TEMPERATURE IN FAHRENHEIT: ");
+    scanf("%d\n", &fahrenheit);
+    
+    finalCel = fahrenheit - 32 * 5/9;
+    printf("IN CELCIUS : %d\n", finalCel);
     return 0;
 }
